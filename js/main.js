e19a8e18a7bda8ab8ab31dc30406a228e3d2e3ef
arthur12345543 <arthur952010@gmail.com>
2020-08-17 18:56:06 +0300

    Adding gallery section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function(){
 	SmoothScroll({ stepSize: 60 });
+	$('.gallery .img-wr').hover(function(){
+		$(this).find(".hover-effect").css("display","block");
+	},function(){
+		$(this).find(".hover-effect").css("display","none");
+	});
 });
