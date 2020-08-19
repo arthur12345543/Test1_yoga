ef6debc5a9d51bd441420664ac58d544828b7c4e
arthur12345543 <arthur952010@gmail.com>
2020-08-20 01:07:35 +0300

    Minor fixes

--- js/main.js
+++ js/main.js
@@ -10,16 +10,34 @@ $(function(){
 	},function(){
 		$(this).find(".hover-effect").css("display","none");
 	});
+	$('.plan-items .content .buy-now1').hover(function(){
+		$(".item1 .title").css("box-shadow","0px 5px 20px 0px rgb(91 108 235)");
+	},function(){
+		$(".item1 .title").css("box-shadow","none");
+	});
+	$('.plan-items .content .buy-now2').hover(function(){
+		$(".item2 .title").css("box-shadow","0px 5px 20px 0px rgb(91 108 235)");
+	},function(){
+		$(".item2 .title").css("box-shadow","none");
+	});
+	$('.plan-items .content .buy-now3').hover(function(){
+		$(".item3 .title").css("box-shadow","0px 5px 20px 0px rgb(91 108 235)");
+	},function(){
+		$(".item3 .title").css("box-shadow","none");
+	});
+	//about us slide
 	$('.carousel').slick({
 		accessibility: true,
 		arrows: true,
 		autoplay: true,
 	});
-	$('.main-testimonial').slick({
+	$('.main-testimonial').slick({//nothing but don't touch while it's working
 		accessibility: true,
 		arrows: true,
 		autoplay: true
 	});
+
+	//slides for testimonials
 	$('.slider-nav').on('init', function(event, slick) {
  		$('.slider-nav .slick-slide.slick-current').addClass('is-active');
  	}).slick({
@@ -30,6 +48,7 @@ $(function(){
  		focusOnSelect: false,
  		infinite: false
 	});
+	
 	$('.main-testimonial').on('afterChange', function(event, slick, currentSlide){
 		$('.slider-nav').slick('slickGoTo', currentSlide);
 		var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
@@ -42,5 +61,11 @@ $(function(){
 		var goToSingleSlide = $(this).data('slick-index');
 
 		$('.main-testimonial').slick('slickGoTo', goToSingleSlide);
+	});
+
+	//smooth scroll all achors links
+	$('a[href*=\\#]').on('click', function(event){     
+	    event.preventDefault();
+	    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 	});
 });
